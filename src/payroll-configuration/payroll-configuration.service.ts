514ca2f4946ec76f037bce45e387215cf3b3be4f
slinder01 <tachack01@gmail.com>
2025-11-29 18:57:08 +0200

    Added payroll manager approve/reject, and create/update allowance

--- src/payroll-configuration/payroll-configuration.service.ts
+++ src/payroll-configuration/payroll-configuration.service.ts
@@ -2,9 +2,15 @@ import { Injectable, ForbiddenException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { payrollPoliciesDocument, payrollPolicies } from './models/payrollPolicies.schema';
+import { allowanceDocument, allowance } from './models/allowance.schema';
 import { CreatePayrollPolicyDto } from './dtos/createPayrollPolicy.dto';
 import { UpdatePayrollPolicyDto } from './dtos/updatePayrollPolicy.dto';
 import { ConfigStatus } from './enums/payroll-configuration-enums';
+import { CreateAllowanceDto } from './dtos/createAllowance.dto';
+import { UpdateAllowanceDto } from './dtos/updateAllowance.dto';
+import { SystemRole } from '../employee-profile/enums/employee-profile.enums';
+
+//import { EmployeeSystemRoleDocument, EmployeeSystemRole } from '../employee-profile/models/employee-system-role.schema';
 
 @Injectable()
 export class PayrollConfigurationService {
@@ -12,6 +18,8 @@ export class PayrollConfigurationService {
      constructor(
     @InjectModel(payrollPolicies.name)
     private policyModel: Model<payrollPoliciesDocument>,
+    @InjectModel(allowance.name)
+    private allowanceModel: Model<allowanceDocument>,
   ) {}
  	 
     //payroll specialist:
@@ -21,24 +29,31 @@ export class PayrollConfigurationService {
         const newPolicy = new this.policyModel({
             ...dto,
             createdBy: userId,
-            //status will be draft by default
+            //status will be draft by default. still:
+            status: ConfigStatus.DRAFT
         });
         return await newPolicy.save();
         
     }
 
-    async updatePayrollPolicy(dto: UpdatePayrollPolicyDto, policyId: string){ 
-        const updatedPolicy = await this.policyModel.findById(policyId);
+    async updatePayrollPolicy(dto: UpdatePayrollPolicyDto, policyId: string, userId: string){
+        
+    
+        const policy = await this.policyModel.findById(policyId);
 
-        if(!updatedPolicy){
+        if(!policy){
             throw new Error('Payroll Policy not found');
         }
-        if(updatedPolicy.status !== ConfigStatus.DRAFT){
+        if(policy.status !== ConfigStatus.DRAFT){
             throw new Error('Only draft policies can be Edited');
         }
 
-        Object.assign(updatedPolicy, dto);
-        return await updatedPolicy.save();
+        if(policy.createdBy?.toString() !== userId){
+            throw new ForbiddenException('You are only allowed to update policies you have created');
+        }
+
+        Object.assign(policy, dto);
+        return await policy.save();
         
     }
 
@@ -70,12 +85,83 @@ export class PayrollConfigurationService {
             throw new ForbiddenException('You are only allowed to submit policies you have created');
         }
         
-       //policy status remains draft until set to pending by maneger, then no editing allowed
+       //policy status remains draft until set to pending by manager, then no editing allowed
         return await policy.save();
         
         // policy.approvalStatus = ApprovalStatus.PENDING;
         // return await policy.save();
     }
+
+    async createAllowance(dto: CreateAllowanceDto, userId: string){
+        const newAllowance = new this.allowanceModel({
+            ...dto,
+            createdBy: userId, //remember to remove created by from dtos
+            status: ConfigStatus.DRAFT //policy status remains draft until set to pending by manager, then no editing allowed
+        })
+        return await newAllowance.save();
+    }
+
+    async updateAllowance(dto: UpdateAllowanceDto, userId: string , allowanceId: string){
+        const allowance = await this.allowanceModel.findById(allowanceId);
+        if(!allowance){
+            throw new Error('Allowance not found');
+        }
+        if(allowance.createdBy?.toString() !== userId){
+            throw new ForbiddenException('You are only allowed to update allowances you have created');
+        }
+        Object.assign(allowance, dto);
+        return await allowance.save();
+    }
+
+    async findAllAllowances(): Promise<allowance[]> {
+        return await this.allowanceModel.find().exec();
+    }
+
+    async findOneAllowance(id: string): Promise<allowance> {
+        const allowance = await this.allowanceModel.findById(id);
+        if(!allowance){
+            throw new Error('Allowance Not Found');
+        }
+        return allowance;
+    }
+
+    //manager:
+    //approve / reject policies
+
+    async approvePolicy(policyId: string, user: any){
+        const policy = await this.policyModel.findById(policyId);
+        if(!policy){
+            throw new Error('Payroll Policy Not Found');
+        }
+        if(user !== SystemRole.PAYROLL_MANAGER){
+            throw new ForbiddenException('Only payroll managers are allowed to approve policies');
+        }
+        policy.status = ConfigStatus.APPROVED;
+        policy.approvedBy = user._id.toString(); //correct later
+        return await policy.save();
+        
+    }
+
+    async rejectPolicy(policyId: string, user: any){
+        const policy = await this.policyModel.findById(policyId);
+        if(!policy){
+            throw new Error('Payroll Policy Not Found');
+        }
+        if(user !== SystemRole.PAYROLL_MANAGER){
+            throw new ForbiddenException('Only payroll managers are allowed to reject policies');
+        }
+        policy.status = ConfigStatus.REJECTED;
+        policy.approvedBy = user._id.toString(); //correct later
+        return await policy.save();
+        
+    }
+
+    
+
+    
+    
+
+
         
 
 
