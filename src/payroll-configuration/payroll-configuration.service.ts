932458c37ca2c1eca5e260387b4f82e3d8e34d99
slinder01 <tachack01@gmail.com>
2025-11-29 17:34:53 +0200

    Corrected submitForApproval function in service layer and reverted schema back to normal

--- src/payroll-configuration/payroll-configuration.service.ts
+++ src/payroll-configuration/payroll-configuration.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { payrollPoliciesDocument, payrollPolicies } from './models/payrollPolicies.schema';
@@ -57,7 +57,26 @@ export class PayrollConfigurationService {
 
 
     //submit for approval. (Note for next time, Use approval-status.enum)
-
+     async submaitForApproval(policyId: string, userId: string){
+        const policy = await this.policyModel.findById(policyId);
+        
+        if(!policy){
+            throw new Error('Payroll Policy Not Found');
+        }
+        if(policy.status !== ConfigStatus.DRAFT){
+            throw new Error('Only draft policies can be submitted for approval');
+        }
+        if(policy.createdBy?.toString() !== userId){
+            throw new ForbiddenException('You are only allowed to submit policies you have created');
+        }
+        
+       //policy status remains draft until set to pending by maneger, then no editing allowed
+        return await policy.save();
+        
+        // policy.approvalStatus = ApprovalStatus.PENDING;
+        // return await policy.save();
+    }
+        
 
 
             
